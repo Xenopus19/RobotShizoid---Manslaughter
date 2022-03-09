7ed8dbadf126fbeb3ec7ae276deb39e4c60b20eb
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-03-09 15:08:11 +0200

    Menu (started)

---
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour {
+
+    [SerializeField] Button StartGameButton = null;
+    [SerializeField] Button SettingsButton = null;
+    [SerializeField] Button QuitGameButton = null;
+    [SerializeField] Button DevelopersButton = null;
+    //[SerializeField] GameObject Developers;
+
+    void Start() {
+        Cursor.visible = (true);
+        Cursor.lockState = CursorLockMode.None;
+        if (StartGameButton != null) {
+            StartGameButton.onClick.AddListener(StartGame);
+        }
+        if (SettingsButton != null) {
+            SettingsButton.onClick.AddListener(StartGame);
+        }
+        if (QuitGameButton != null) {
+            QuitGameButton.onClick.AddListener(QuitGame);
+        }
+        if (DevelopersButton != null) {
+            DevelopersButton.onClick.AddListener(OpenDevelopers);
+        }
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Arena");
+    }
+
+    public void OpenDevelopers() {
+        //Developers.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void CloseDevelopers() {
+        //Developers.SetActive(false);
+        gameObject.SetActive(true);
+    }
+}
