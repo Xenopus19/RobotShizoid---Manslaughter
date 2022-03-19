448986d35dc999b475cdd13fcc4c09c3c7f60ffc
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-03-19 19:26:17 +0200

    diagonal walking

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour {
     public float moveSpeed = 10f;
+    private bool IsWalking = false;
     [SerializeField] private List<float> EdgeOfArena = new List<float>();
 
     [SerializeField] private Animator _animator;
@@ -11,20 +12,22 @@ public class PlayerMovement : MonoBehaviour {
     private void Update() => CheckMove();
 
     public void CheckMove() {
+        IsWalking = false;
         if (Input.GetKey(KeyCode.UpArrow) && transform.position.z < EdgeOfArena[0] - 0.1f)
             Walk(Vector3.forward, "IsUp");
-        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.z > EdgeOfArena[1] + 0.1f)
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.z > EdgeOfArena[1] + 0.1f)
             Walk(Vector3.back, "IsDown");
-        else if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < EdgeOfArena[2] - 0.1f)
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < EdgeOfArena[2] - 0.1f)
             Walk(Vector3.right, "IsRight");
-        else if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > EdgeOfArena[3] + 0.1f)
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > EdgeOfArena[3] + 0.1f)
             Walk(Vector3.left, "IsLeft");
-        else
-            Stand();
+
+        if (!IsWalking) Stand();
     }
 
     private void Walk(Vector3 direction, string anim) {
         transform.Translate(direction * moveSpeed * Time.deltaTime);
+        IsWalking = true;
         _animator.SetBool(anim, true);
         if (!_audioSource.isPlaying)
             _audioSource.Play();
