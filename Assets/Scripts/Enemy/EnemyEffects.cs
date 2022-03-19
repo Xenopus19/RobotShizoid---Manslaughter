6813bcbc86c624f037c5c509642709e47df4a6f8
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2022-03-19 13:34:15 +0200

    Enemy blood effects

--- Assets/Scripts/Enemy/EnemyEffects.cs
+++ Assets/Scripts/Enemy/EnemyEffects.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class EnemyEffects : MonoBehaviour
 {
+    [SerializeField] private Transform FloorBloodOrigin;
+
     [SerializeField] private GameObject RagdollPrefab;
+    [SerializeField] private GameObject MeatChunkParticle;
+    [SerializeField] private GameObject[] FloorBloodVariants;
     private void Start()
     {
-        GetComponent<EnemyHealth>().OnDeath += CreateDeathEffects;
+        EnemyHealth health = GetComponent<EnemyHealth>();
+        health.OnDeath += CreateDeathEffects;
+        health.OnHealthChanged += CreateBloodSplashes;
+    }
+
+    private void CreateBloodSplashes()
+    {
+        Instantiate(MeatChunkParticle, transform);
     }
 
     private void CreateDeathEffects()
     {
+        Instantiate(FloorBloodVariants[Random.Range(0, FloorBloodVariants.Length - 1)], FloorBloodOrigin.position, Quaternion.identity);
         GameObject Corpse = Instantiate(RagdollPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
-
     }   
 }
