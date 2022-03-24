2bb1a1d386f521780c9b1109ea45f0d523397e95
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2022-03-24 12:49:55 +0200

    New enemy type

---
+++ Assets/Scripts/Enemy/EnemyBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] private float Damage;
+    [SerializeField] private float Speed;
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * Speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerHealth health = collision.gameObject.GetComponent<PlayerHealth>();
+
+        if(health!=null)
+        {
+            health.GetDamage(Damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
