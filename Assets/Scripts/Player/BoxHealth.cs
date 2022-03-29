55943e52f40c0aa6141751239f0b2eb204464c04
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-03-29 19:40:56 +0300

    Explode effects for box

--- Assets/Scripts/Player/BoxHealth.cs
+++ Assets/Scripts/Player/BoxHealth.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class BoxHealth : MonoBehaviour {
     public float Recovery;
+    [SerializeField] GameObject Explosion;
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player") {
             collision.gameObject.GetComponent<PlayerHealth>().RecoverHealth(Recovery);
             Destroy(gameObject);
         }
+    }
+    private void OnDestroy() {
+        Instantiate(Explosion, transform.position, Quaternion.identity);
     }
 }
