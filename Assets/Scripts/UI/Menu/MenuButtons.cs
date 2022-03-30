de8fdf0d194e84d4c6e280268705ceaab3c1d12b
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-03-30 10:12:17 +0300

    box chance fixed

--- Assets/Scripts/UI/Menu/MenuButtons.cs
+++ Assets/Scripts/UI/Menu/MenuButtons.cs
@@ -53,5 +53,4 @@ public class MenuButtons : MonoBehaviour {
 
     public void QuitGame() => Application.Quit();
     public void PlayHoleSound() => _audioSource.Play();
-    public void OpenTutorial() => SceneManager.LoadScene("Tutorial");
 }
