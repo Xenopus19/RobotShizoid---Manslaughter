a1b50971efcb1a3cbfdf8181a1aae33da87cef7c
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-04-02 16:35:34 +0300

    Tutorial changes

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -2,22 +2,19 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InputController : MonoBehaviour
-{
+public class InputController : MonoBehaviour {
     private GameObject Player;
     private PlayerWeapons Weapons;
 
     [SerializeField] private Button ButtonAttack;
     [SerializeField] private Button ButtonWeaponChange;
-    private void Start()
-    {
+    private void Start() {
         Player = GameObject.FindGameObjectWithTag("Player");
         Weapons = Player.GetComponent<PlayerWeapons>();
         AddListenerToButtons();
     }
 
-    private void AddListenerToButtons()
-    {
+    private void AddListenerToButtons() {
         if (ButtonAttack != null)
             ButtonAttack.onClick.AddListener(Attack);
         if (ButtonWeaponChange != null)
@@ -26,29 +23,8 @@ public class InputController : MonoBehaviour
 
     public void Attack() => Weapons.DoAttack();
 
-    public void ChangeWeapon()
-    {
-        /*if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Weapons.ChangeWeapon(0);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Weapons.ChangeWeapon(1);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Weapons.ChangeWeapon(2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Weapons.ChangeWeapon(3);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            Weapons.ChangeWeapon(4);
-        }*/
+    public void ChangeWeapon() {
+        print("change");
         Weapons.NextWeapon();
-
     }
 }
