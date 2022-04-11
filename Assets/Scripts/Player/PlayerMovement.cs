db3438089b6251ea8e35ec7c202839fffc187b72
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2022-04-11 11:24:31 +0300

    MergeFixed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,34 +1,40 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float moveSpeed = 10f;
+    public bool IsWalking = false;
     [SerializeField] private List<float> EdgeOfArena = new List<float>();
 
     [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _audioSource;
 
-    public void Move(MoveType moveType) {
+    public void Move(MoveType moveType = MoveType.Stand)
+    {
         if (moveType == MoveType.Up && transform.position.z < EdgeOfArena[0] - 0.1f)
             Walk(Vector3.forward, "IsUp");
-        else if (moveType == MoveType.Down && transform.position.z > EdgeOfArena[1] + 0.1f)
+        if (moveType == MoveType.Down && transform.position.z > EdgeOfArena[1] + 0.1f)
             Walk(Vector3.back, "IsDown");
-        else if (moveType == MoveType.Right && transform.position.x < EdgeOfArena[2] - 0.1f)
+        if (moveType == MoveType.Right && transform.position.x < EdgeOfArena[2] - 0.1f)
             Walk(Vector3.right, "IsRight");
-        else if (moveType == MoveType.Left && transform.position.x > EdgeOfArena[3] + 0.1f)
+        if (moveType == MoveType.Left && transform.position.x > EdgeOfArena[3] + 0.1f)
             Walk(Vector3.left, "IsLeft");
-        else
-            Stand();
+
+        if (moveType == MoveType.Stand) Stand();
     }
 
-    private void Walk(Vector3 direction, string anim) {
+    private void Walk(Vector3 direction, string anim)
+    {
         transform.Translate(direction * moveSpeed * Time.deltaTime);
+        IsWalking = true;
         _animator.SetBool(anim, true);
         if (!_audioSource.isPlaying)
             _audioSource.Play();
     }
 
-    private void Stand() {
+    private void Stand()
+    {
         _animator.SetBool("IsUp", false);
         _animator.SetBool("IsDown", false);
         _animator.SetBool("IsRight", false);
@@ -37,8 +43,8 @@ public class PlayerMovement : MonoBehaviour {
             _audioSource.Stop();
     }
 }
-
-public enum MoveType {
+public enum MoveType
+{
     Up,
     Down,
     Right,
