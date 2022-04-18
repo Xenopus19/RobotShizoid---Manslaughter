501c8925e341e54f6449b4a18301ab7715091bce
Xenopus19 <moskovskaya.olga10@gmail.com>
2022-04-18 15:35:12 +0300

    New Weapon 1

--- Assets/Scripts/Weapons/SphereMelee.cs
+++ Assets/Scripts/Weapons/SphereMelee.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class SphereMelee : Weapon
 {
     [SerializeField] public float AttackRadius;
+    [SerializeField] public float ForwardShift;
+    
     
 
     public override Collider[] GetAttackedColliders(Vector3 AttackPosition)
     {
+        AttackPosition.z += ForwardShift;
         Collider[] AttackedColliders = Physics.OverlapSphere(AttackPosition, AttackRadius);
         return AttackedColliders;
     }
