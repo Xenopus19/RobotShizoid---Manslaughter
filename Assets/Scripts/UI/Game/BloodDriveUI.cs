61f23e571cd3f75f473e730db9350770059c1c1a
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-05-21 12:30:42 +0300

    BGM per arena

--- Assets/Scripts/UI/Game/BloodDriveUI.cs
+++ Assets/Scripts/UI/Game/BloodDriveUI.cs
@@ -1,18 +1,21 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BloodDriveUI : MonoBehaviour {
+public class BloodDriveUI : MonoBehaviour
+{
     [SerializeField] private GameObject driveEffects;
-    [SerializeField] private AudioSource BGM;
+    [SerializeField] private List<GameObject> BGMG = new List<GameObject>();
 
     private GameObject Player;
     private Image BloodDriveScale;
     private BloodDrive bloodDrive;
     private AudioSource audioSource;
-    
 
-    void Start() {
+
+    void Start()
+    {
         BloodDriveScale = GetComponent<Image>();
 
         Player = GameObject.FindGameObjectWithTag("Player");
@@ -23,20 +26,32 @@ public class BloodDriveUI : MonoBehaviour {
         UpdateUI();
     }
 
-    public void UpdateUI() {
+    public void UpdateUI()
+    {
         BloodDriveScale.fillAmount = bloodDrive.GetDriveFillPercent();
     }
 
     private void BeginDriveEffects(float DriveTime)
     {
+        AudioSource BGM = DefineBGM();
         driveEffects.SetActive(true);
         BGM.pitch = 0.5f;
         BGM.spatialBlend = 0.5f;
 
-        StartCoroutine(FinishEffects(DriveTime));
+        StartCoroutine(FinishEffects(DriveTime, BGM));
     }
 
-    private IEnumerator FinishEffects(float DriveTime)
+    private AudioSource DefineBGM()
+    {
+        for (int i = 0; i < BGMG.Count; i++)
+        {
+            if (BGMG[i].activeInHierarchy)
+                return BGMG[i].GetComponent<AudioSource>();
+        }
+        return null;
+    }
+
+    private IEnumerator FinishEffects(float DriveTime, AudioSource BGM)
     {
         yield return new WaitForSeconds(DriveTime);
         BGM.pitch = 1;
